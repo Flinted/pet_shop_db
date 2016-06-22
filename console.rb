c4762a06e1150804f724fb5491f777ee1dc8f082
Chris Flint <flintedc@gmail.com>
2016-06-22 15:51:14 +0100

    added all functions to pets and shops

--- console.rb
+++ console.rb
@@ -5,6 +5,8 @@ runner = SqlRunner.new({dbname: 'pet_store', host: 'localhost'})
 pet_store1 = PetShop.new({'name' => 'Turtles R us', 'address' => '15 wet street', 'stock_type' => 'aquatic'}, runner)
 pet_store2 = PetShop.new({'name' => 'Monkey Business', 'address' => '85 the tree tops', 'stock_type' => 'monkeys'}, runner)
 
+pet_store1.save()
+pet_store2.save()
 
 pet1 = Pet.new({'name' => 'Malcolm', 'type' => 'turtle', 'store_id' => pet_store1.id}, runner)
 pet2 = Pet.new({'name' => 'Roger', 'type' => 'terrapin', 'store_id' => pet_store1.id}, runner)
@@ -12,3 +14,10 @@ pet3 = Pet.new({'name' => 'Susan', 'type' => 'gorilla', 'store_id' => pet_store2
 pet4 = Pet.new({'name' => 'Steve', 'type' => 'orangutan', 'store_id' => pet_store2.id}, runner)
 pet5 = Pet.new({'name' => 'Reginald', 'type' => 'gecko', 'store_id' => pet_store1.id}, runner)
 pet6 = Pet.new({'name' => 'Louis', 'type' => 'chimp', 'store_id' => pet_store2.id}, runner)
+
+pet1.save()
+pet2.save()
+pet3.save()
+pet4.save()
+pet5.save()
+pet6.save()
--- models/pet.rb
+++ models/pet.rb
@@ -11,7 +11,20 @@ class Pet
   def save()
     sql = "INSERT INTO pets (name, type, store_id) VALUES ('#{@name}', '#{@type}','#{@store_id}') RETURNING *"
     pet_data = @runner.run(sql)
+    @id = pet_data.first['id'].to_i 
     return pet_data
   end
 
+  def self.all(runner)
+    sql = "SELECT * FROM pets"
+    return_info = runner.run(sql)
+    pets = return_info.map {|pet| Pet.new(pet, runner)}
+    return pets
+  end
+
+  def show_store()
+    sql = "SELECT * FROM pet_shops WHERE id = #{@id}"
+    store = @runner.run(sql).first
+    return store['name']
+  end
 end
